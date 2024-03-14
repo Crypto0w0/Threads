bdd0d4ce27264f1fc66a955f0c562bfdc0c0bef7
Alex Untilov <93001397+Crypto0w0@users.noreply.github.com>
2024-03-15 00:24:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+static class Program
+{
+    static int start;
+    static int end;
+    static void Init()
+    {
+        try
+        {
+            start = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (TypeInitializationException)
+        {
+            start = 0;
+        }
+        try
+        {
+            end = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (TypeInitializationException)
+        {
+            end = -1;
+        }
+    }
+    
+    static Thread t1 = new Thread(() => GenNum(start, end));
+    
+    static void Main()
+    {
+        Init();
+        t1.Start();
+        while (t1.IsAlive)
+        {
+            string c = Console.ReadLine();
+            if (c == "s")
+            {
+                t1.Abort();
+            }
+        }
+    }
+    static void GenNum(int s, int e)
+    {
+        int n = 0;
+        if (e == -1)
+        {
+            while(true)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+        if (s == 0)
+        {
+            for (int i = s; i <= e; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        t1.Abort();
+    }
+}
